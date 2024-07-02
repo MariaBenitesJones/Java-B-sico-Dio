c47cb9a78b9c06f66eb2b7793e717f23598f2243
MariaBenitesJones <mariaeduardabmrj@gmail.com>
2024-07-02 16:15:22 -0300

    Meu primeiro projeto java

---
+++ meu-primeiro-projeto-java/src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Seja bem-vindo a minha nova vida, que todos saibam que Deus me deu");
+    }
+}
